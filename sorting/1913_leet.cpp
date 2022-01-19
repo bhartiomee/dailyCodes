c44d95e87029bb69ca2b47c30ad19e4ee6bba84e
bhartiomee <bhartiomee25@gmail.com>
2022-01-20 01:05:13 +0530

    Codes

--- sorting/1913_leet.cpp
+++ sorting/1913_leet.cpp
@@ -4,3 +4,6 @@ int maxProductDifference(vector<int> &nums)
   int n = nums.size();
   return ((nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]));
 }
+
+//max diff will of a smallest number and a largest number
+// and smallest product will of two small number and same for largest numbers
