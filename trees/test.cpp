e2c135500fe53dc842bb030e2bd582dfbab84b72
bhartiomee <bhartiomee25@gmail.com>
2022-01-28 22:30:06 +0530

    codes

--- trees/test.cpp
+++ trees/test.cpp
@@ -1,30 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-  int n;
-  cin >> n;
-  int cycle = 1;
-  int i = cycle;
-  int total = 0;
-  int sum = 0;
-  int cnt=0;
-  while (cnt < n)
+  // your code goes here
+  int t;
+  cin >> t;
+  while (t--)
   {
-    if (cnt>0 && (cnt)%8 == 0)
-    {
-      total+=sum;
-      cout<<cycle<<endl;
-      cycle++;
-      i=cycle;
-      sum=0;
-    }
-    cnt++;
-      sum+=i;
-      cout<<i<<" ";
-      i++;
-    
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    int cp = x * y;
+    int sp = x * z;
+    int profit = sp - cp;
+    cout << profit << endl;
   }
-  cout<<total;
   return 0;
 }
