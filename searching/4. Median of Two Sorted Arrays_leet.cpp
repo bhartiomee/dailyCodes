cd15474fb9f95249dd45ac0e035ab73983429839
bhartiomee <bhartiomee25@gmail.com>
2022-03-04 22:06:22 +0530

    code

--- searching/4. Median of Two Sorted Arrays_leet.cpp
+++ searching/4. Median of Two Sorted Arrays_leet.cpp
@@ -6,14 +6,11 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if(n2<n1)return findMedianSortedArrays(nums2,nums1);
         int low=0;
         int high=n1;
-        
         while(low<=high){
             int cut1=(low+high)/2;
             int cut2=(n1+n2+1)/2-cut1;
-            
             int l1=cut1==0?INT_MIN:nums1[cut1-1];
             int l2=cut2==0?INT_MIN:nums2[cut2-1];
-            
             int r1=cut1==n1?INT_MAX:nums1[cut1];
             int r2=cut2==n2?INT_MAX:nums2[cut2];
             
