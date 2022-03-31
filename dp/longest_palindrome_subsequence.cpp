f0582a56fcb91448f082916a9887de7af9b59a19
bhartiomee <bhartiomee25@gmail.com>
2022-03-31 18:27:28 +0530

    Codes

---
+++ dp/longest_palindrome_subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution
+{
+public:
+  int longestPalindromeSubseq(string b)
+  {
+
+    string a = b;
+    reverse(b.begin(), b.end());
+    return lcs(a, b);
+  }
+
+  int lcs(string a, string b)
+  {
+    int n = a.length();
+
+    int dp[n + 1][n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+      for (int j = 0; j <= n; j++)
+      {
+        if (i == 0 || j == 0)
+          dp[i][j] = 0;
+      }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= n; j++)
+      {
+        if (a[i - 1] == b[j - 1])
+        {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        }
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+    return dp[n][n];
+  }
+};
